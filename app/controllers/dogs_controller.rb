359f3bcd70698722f8b800a62116647457cac99e
Samantha Aleman <samanthaaleman@samanthas-air.home>
2020-09-15 21:15:58 -0700

    added changes to controllers and started on my views

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -9,7 +9,7 @@ class DogsController < ApplicationController
         end
     end 
 
-    get '/dogs/new'
+    get '/dogs/new' do
     if logged_in? 
         erb :'dogs/add'
     else
@@ -18,7 +18,18 @@ class DogsController < ApplicationController
 end 
 
     post '/dogs' do 
-    
+        if logged_in? 
+            if params[:]
+                redirect to '/dogs/add'
+            else 
+                @dogs = current_user.dogs.build()
+                if @dogs.save 
+                    redirect to '/dogs/#{dogs.id}'
+                end
+            end 
+        else 
+            redirect to '/login'
+        end
         end
 
     get '/dogs/:id' do
@@ -34,13 +45,16 @@ end
         if logged_in?
             @dogs = Dogs.find_by_id(params[:id])
             if @dogs && @dog.user == current_user
-                erb :'dogs/show'
+                erb :'dogs/edit'
             else
-                redirect to 'login'
-            end 
+                redirect to '/dogs'
+        end 
+            else 
+            redirect to '/login'
         end 
 
     patch '/dogs/:id' do 
+        
     end 
 
     delete '/dogs/:id/delete'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ end
 
     post '/login' do 
         user = User.find_by(:username => params[:username])
+        
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
             redirect to '/dogs'
